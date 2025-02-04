ec354767d879caac92984007e505bd283c7dd34c
AlejaCamperos <aleja.camperos@gmail.com>
2025-02-04 11:49:33 -0500

    Funcion agregar & campo vacio

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 
 // Paso 1: Crear un array vacío para almacenar los nombres
 let listaDeAmigos = [];
+
+/// Paso 2: Función para agregar un amigo a la lista
+function agregarAmigo() {
+    // Obtener el valor del input
+    let inputNombre = document.getElementById("amigo");
+    let nombre = inputNombre.value.trim(); // Eliminar espacios al inicio y final
+
+    console.log("Intentando agregar:", nombre); // Verificar qué nombre se está intentando agregar
+
+    // Validar si el campo está vacío
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+}
